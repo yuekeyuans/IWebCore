207b2c75155557c3d87f4157071b48b0423ab217
yuekeyuan <yuekeyuan@tom.com>
2022-07-15 00:00:25 +0800

    布置一下明天的任务

--- src/web/response/IResponseWareRaw.h
+++ src/web/response/IResponseWareRaw.h
@@ -11,6 +11,15 @@ public:
     void setMime(IHttpMime mime);
     void setMime(const QString& mime);
 
+    // TODO: 添加 setContent;
+    void setContent(const QString& content);
+    void setContent(const QByteArray& content);
+    void setContent(const char* content);
+
+    void setContent(const QFileInfo& content);
+    void setContent(const QFile& file); // TODO: 这个不一定行， 得具体测试一下看看
+    void setFileContent(const QString& filePath);
+
 public:
     QString mimeString{"UNKNOWN"};   // when mime is unknown and mimeString is not empty ,mimeString take effect.
 
