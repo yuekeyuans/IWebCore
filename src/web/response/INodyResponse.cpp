af80880b5e982b610305963eb7797122d5b70fde
yuekeyuan <yuekeyuan@tom.com>
2024-03-25 00:45:49 +0800

    submit code

--- src/web/response/INodyResponse.cpp
+++ src/web/response/INodyResponse.cpp
@@ -10,7 +10,7 @@ INodyResponse::INodyResponse(const QString &path, QJsonValue value)
     if(nody != nullptr){
         setMime(IHttpMime::TEXT_HTML_UTF8);
         QMap<QString, QJsonValue> map;
-        m_raw->setContent(nody->operator ()(value, map));
+        m_raw->setContent(nody->execute(value, map));
     }else{
         setInvalid(IHttpStatus::INTERNAL_SERVER_ERROR_500, "template can not parse template:" + path);
     }
