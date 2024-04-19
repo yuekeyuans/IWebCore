0c640bd6e9534adb5b70c58ceebe2044428e69aa
yuekeyuan <yuekeyuan@tom.com>
2024-04-20 00:49:48 +0800

    验证测试案例

--- test/TestCores/TestTestModule/ControllerTest.cpp
+++ test/TestCores/TestTestModule/ControllerTest.cpp
@@ -8,19 +8,18 @@ ControllerTest::ControllerTest()
 {
 }
 
+// TODO: crash does no matter, because it is the test.
 void ControllerTest::a()
 {
-    try
-    {
-        // you can pass http::InternetProtocol::V6 to Request to make an IPv6 request
+//    try
+//    {
         http::Request request{"http://127.0.0.1:8550/test"};
-        // send a get request
         const auto response = request.send("GET");
         auto data = QString::fromStdString(std::string{response.body.begin(), response.body.end()}); // print the result
         QVERIFY(data == "hello world");
-    }
-    catch (const std::exception& e)
-    {
-        qDebug() << "Request failed, error: " << e.what() << '\n';
-    }
+//    }
+//    catch (const std::exception& e)
+//    {
+//        qDebug() << "Request failed, error: " << e.what() << '\n';
+//    }
 }
