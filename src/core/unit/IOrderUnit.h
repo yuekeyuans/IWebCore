7064e027d071f146062be9dbdd60f818e850af8a
yuekeyuan <yuekeyuan@tom.com>
2024-05-03 14:02:45 +0800

    suBmit code

--- src/core/unit/IOrderUnit.h
+++ src/core/unit/IOrderUnit.h
@@ -9,21 +9,31 @@ struct IOrderUnit
 {
 public:
     virtual QString name() const = 0;
-    virtual double order() const { return 50; }
+    virtual double order() const;
 };
 
-namespace IOrderUnitUtil{
+namespace IOrderUnitUtil
+{
     template<typename T>
-    static void sortUnit(QList<T*>& values)
-    {
-        std::sort(values.begin(), values.end(), [](T* lhs, T* rhs) -> bool{
-            if(lhs->order() != rhs->order()){
-                return lhs->order() < rhs->order();
-            }
-
-            return lhs->name() < rhs->name();
-        });
-    }
+    static void sortUnit(QList<T*>& values);
 }
+
+double IOrderUnit::order() const
+{
+    return 50;
+}
+
+template<typename T>
+static void IOrderUnitUtil::sortUnit(QList<T*>& values)
+{
+    std::sort(values.begin(), values.end(), [](T* lhs, T* rhs) -> bool
+    {
+        if(lhs->order() != rhs->order()){
+            return lhs->order() < rhs->order();
+        }
+        return lhs->name() < rhs->name();
+    });
+}
+
 
 $PackageWebCoreEnd
