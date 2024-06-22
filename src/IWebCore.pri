33dc5318b025defd6a3a3e4c69ce2d9f8768bc54
yuekeyuan <yuekeyuan@tom.com>
2024-06-22 19:27:12 +0800

    submit code

--- src/IWebCore.pri
+++ src/IWebCore.pri
@@ -205,7 +205,6 @@ HEADERS += \
     $$PWD/http/response/IResponseTemplateRenderer.h \
     $$PWD/http/response/IResponseWare.h \
     $$PWD/http/response/response \
-    $$PWD/http/controller/pp/IControlVariablePreProcessor.h \
     $$PWD/http/controller/pp/IControllerPreProcessor.h \
     $$PWD/http/controller/pp/IDeleteMappingPreProcessor.h \
     $$PWD/http/controller/pp/IGetMappingPreProcessor.h \
--- src/http/controller/pp/IControllerPreProcessor.h
+++ src/http/controller/pp/IControllerPreProcessor.h
@@ -8,3 +8,31 @@ private:
 
 #define PP_CONTROLLER_PROP(funName, type, index)    \
     PP_STRING(iwebControllerFun ## $ ## funName ## $ ## type ## $ ## index)
+
+
+#ifdef Q_MOC_RUN
+    #define $Mixed(type, name)      type name##_mixed
+    #define $Param(type, name)      type name##_param
+    #define $Url(type, name)        type name##_url
+    #define $Body(type, name)       type name##_body
+    #define $Content(type, name)    type name##_content
+    #define $Header(type, name)     type name##_header
+    #define $Cookie(type, name)     type name##_cookie
+    #define $Session(type, name)    type name##_session
+    #define $Nullable(type, name)   type name##_nullable
+    #define $NotNull(type, name)    type name$$_notnull
+
+#else
+    #define $Mixed(type, name)      type name
+    #define $Param(type, name)      type name
+    #define $Url(type, name)        type name
+    #define $Body(type, name)       type name
+    #define $Content(type, name)    type name
+    #define $Header(type, name)     type name
+    #define $Cookie(type, name)     type name
+    #define $Session(type, name)    type name
+    #define $Nullable(type, name)   type name
+    #define $NotNull(type, name)    type name
+
+#endif
+
