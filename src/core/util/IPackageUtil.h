ff66479a2ac0da41aff895304f5edff591c819c4
yuekeyuan <yuekeyuan@tom.com>
2024-07-05 23:18:52 +0800

    修改命名

--- src/core/util/IPackageUtil.h
+++ src/core/util/IPackageUtil.h
@@ -3,32 +3,32 @@
 #include "core/util/IPreProcessorUtil.h"
 
 // NOTE: this writing will be greatly improve in cpp20 latter
-#define $PackageBegin_1(name1) \
+#define $IPackageBegin_1(name1) \
 inline namespace name1 {
 
-#define $PackageBegin_2(name1, name2) \
+#define $IPackageBegin_2(name1, name2) \
 inline namespace name1 {   \
     inline namespace name2 {
 
-#define $PackageBegin_3(name1, name2, name3) \
+#define $IPackageBegin_3(name1, name2, name3) \
 inline namespace name1 {   \
     inline namespace name2 {   \
         inline namespace name3
 
-#define $PackageBegin_4(name1, name2, name3, name4) \
+#define $IPackageBegin_4(name1, name2, name3, name4) \
 inline namespace name1 {   \
     inline namespace name2 {   \
         inline namespace name3 \
             inline namespace name4
 
-#define $PackageBegin_5(name1, name2, name3, name4, name5) \
+#define $IPackageBegin_5(name1, name2, name3, name4, name5) \
 inline namespace name1 {   \
     inline namespace name2 {   \
         inline namespace name3 \
             inline namespace name4 \
                 inline namespace name5
 
-#define $PackageBegin_6(name1, name2, name3, name4, name5, name6) \
+#define $IPackageBegin_6(name1, name2, name3, name4, name5, name6) \
 inline namespace name1 {   \
     inline namespace name2 {   \
         inline namespace name3 \
@@ -36,7 +36,7 @@ inline namespace name1 {   \
                 inline namespace name5 \
                     inline namespace name6
 
-#define $PackageBegin_7(name1, name2, name3, name4, name5, name6, name7) \
+#define $IPackageBegin_7(name1, name2, name3, name4, name5, name6, name7) \
 inline namespace name1 {   \
     inline namespace name2 {   \
         inline namespace name3 \
@@ -45,7 +45,7 @@ inline namespace name1 {   \
                     inline namespace name6 \
                         inline namespace name7
 
-#define $PackageBegin_8(name1, name2, name3, name4, name5, name6, name7, name8) \
+#define $IPackageBegin_8(name1, name2, name3, name4, name5, name6, name7, name8) \
 inline namespace name1 {   \
     inline namespace name2 {   \
         inline namespace name3 \
@@ -55,7 +55,7 @@ inline namespace name1 {   \
                         inline namespace name7 \
                             inline namespace name8
 
-#define $PackageBegin_9(name1, name2, name3, name4, name5, name6, name7, name8, name9) \
+#define $IPackageBegin_9(name1, name2, name3, name4, name5, name6, name7, name8, name9) \
 inline namespace name1 {   \
     inline namespace name2 {   \
         inline namespace name3 \
@@ -66,38 +66,36 @@ inline namespace name1 {   \
                             inline namespace name8 \
                                 inline namespace name9
 
-#define $PackageBegin_(N) $PackageBegin_##N
-#define $PackageBegin_EVAL(N) $PackageBegin_(N)
-#define $PackageBegin(...) PP_EXPAND( $PackageBegin_EVAL(PP_EXPAND( PP_NARG(__VA_ARGS__) ))(__VA_ARGS__) )
+#define $IPackageBegin_(N) $IPackageBegin_##N
+#define $IPackageBegin_EVAL(N) $IPackageBegin_(N)
+#define $IPackageBegin(...) PP_EXPAND( $IPackageBegin_EVAL(PP_EXPAND( PP_NARG(__VA_ARGS__) ))(__VA_ARGS__) )
 
-
-// $PackageEnd
-#define $PackageEnd_1(name1) \
+#define $IPackageEnd_1(name1) \
 }
 
-#define $PackageEnd_2(name1, name2) \
+#define $IPackageEnd_2(name1, name2) \
     }   \
 }
 
-#define $PackageEnd_3(name1, name2, name3) \
+#define $IPackageEnd_3(name1, name2, name3) \
         }   \
     }   \
 }
 
-#define $PackageEnd_4(name1, name2, name3, name4) \
+#define $IPackageEnd_4(name1, name2, name3, name4) \
             }   \
         }   \
     }   \
 }
 
-#define $PackageEnd_5(name1, name2, name3, name4, name5) \
+#define $IPackageEnd_5(name1, name2, name3, name4, name5) \
                 }   \
             }   \
         }   \
     }   \
 }
 
-#define $PackageEnd_6(name1, name2, name3, name4, name5, name6) \
+#define $IPackageEnd_6(name1, name2, name3, name4, name5, name6) \
                     }   \
                 }   \
             }   \
@@ -105,7 +103,7 @@ inline namespace name1 {   \
     }   \
 }
 
-#define $PackageEnd_7(name1, name2, name3, name4, name5, name6, name7) \
+#define $IPackageEnd_7(name1, name2, name3, name4, name5, name6, name7) \
                         }   \
                     }   \
                 }   \
@@ -114,7 +112,7 @@ inline namespace name1 {   \
     }   \
 }
 
-#define $PackageEnd_8(name1, name2, name3, name4, name5, name6, name7, name8) \
+#define $IPackageEnd_8(name1, name2, name3, name4, name5, name6, name7, name8) \
                             }   \
                         }   \
                     }   \
@@ -124,7 +122,7 @@ inline namespace name1 {   \
     }   \
 }
 
-#define $PackageEnd_9(name1, name2, name3, name4, name5, name6, name7, name8, name9) \
+#define $IPackageEnd_9(name1, name2, name3, name4, name5, name6, name7, name8, name9) \
                                 }   \
                             }   \
                         }   \
@@ -135,9 +133,9 @@ inline namespace name1 {   \
     }   \
 }
 
-#define $PackageEnd_(N) $PackageEnd_##N
-#define $PackageEnd_EVAL(N) $PackageEnd_(N)
-#define $PackageEnd(...) PP_EXPAND( $PackageEnd_EVAL(PP_EXPAND( PP_NARG(__VA_ARGS__) ))(__VA_ARGS__) )
+#define $IPackageEnd_(N) $IPackageEnd_##N
+#define $IPackageEnd_EVAL(N) $IPackageEnd_(N)
+#define $IPackageEnd(...) PP_EXPAND( $IPackageEnd_EVAL(PP_EXPAND( PP_NARG(__VA_ARGS__) ))(__VA_ARGS__) )
 
 
 #define $PackageUsing_1(name1) \
@@ -172,9 +170,9 @@ inline namespace name1 {   \
 #define $PackageUsing(...) PP_EXPAND( $PackageUsing_EVAL(PP_EXPAND( PP_NARG(__VA_ARGS__) ))(__VA_ARGS__))
 
 #define $PackageWebCoreBegin \
-    $PackageBegin(IWebCore)
+    $IPackageBegin(IWebCore)
 
 #define $PackageWebCoreEnd \
-    $PackageEnd(IWebCore)
+    $IPackageEnd(IWebCore)
 
 #define $PackageWebCoreName "IWebCore"
