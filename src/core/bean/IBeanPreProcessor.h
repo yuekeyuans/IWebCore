43d44ae38c5fb7a0b5e03b1998ec12992ea44db8
yuekeyuan <yuekeyuan@tom.com>
2024-08-11 12:42:21 +0800

    submit code

--- src/core/bean/IBeanPreProcessor.h
+++ src/core/bean/IBeanPreProcessor.h
@@ -56,8 +56,6 @@ public:\
     }   \
 private:
 
-
-
 #define $AsBean(klassName) \
 public: \
     $_UseGadget() \
@@ -92,6 +90,50 @@ private:
 #define $BeanField_3(type, name, value) \
     $BeanFieldDeclare(type, name) \
     type name {value};
+
+#define $BeanField_3(type, name, value) \
+    $BeanFieldDeclare(type, name) \
+    type name {value};
+
+#define $BeanField_3(type, name, value1) \
+    $BeanFieldDeclare(type, name) \
+    type name {value1};
+
+#define $BeanField_4(type, name, value1, value2) \
+    $BeanFieldDeclare(type, name) \
+    type name {value1, value2};
+
+#define $BeanField_5(type, name, value1, value2, value3) \
+    $BeanFieldDeclare(type, name) \
+    type name {value1, value2, value3};
+
+#define $BeanField_6(type, name, value1, value2, value3, value4) \
+    $BeanFieldDeclare(type, name) \
+    type name {value1, value2, value3, value4};
+
+#define $BeanField_7(type, name, value1, value2, value3, value4, value5) \
+    $BeanFieldDeclare(type, name) \
+    type name {value1, value2, value3, value4, value5};
+
+#define $BeanField_8(type, name, value1, value2, value3, value4, value5, value6) \
+    $BeanFieldDeclare(type, name) \
+    type name {value1, value2, value3, value4, value5, value6};
+
+#define $BeanField_9(type, name, value1, value2, value3, value4, value5, value6, value7) \
+    $BeanFieldDeclare(type, name) \
+    type name {value1, value2, value3, value4, value5, value6, value7};
+
+#define $BeanField_10(type, name, value1, value2, value3, value4, value5, value6, value7, value8) \
+    $BeanFieldDeclare(type, name) \
+    type name {value1, value2, value3, value4, value5, value6, value7, value8};
+
+#define $BeanField_11(type, name, value1, value2, value3, value4, value5, value6, value7, value8, value9) \
+    $BeanFieldDeclare(type, name) \
+    type name {value1, value2, value3, value4, value5, value6, value7, value8, value9};
+
+#define $BeanField_12(type, name, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10) \
+    $BeanFieldDeclare(type, name) \
+    type name {value1, value2, value3, value4, value5, value6, value7, value8, value9, value10};
 
 #define $BeanField_(N) $BeanField_##N
 #define $BeanField_EVAL(N) $BeanField_(N)
