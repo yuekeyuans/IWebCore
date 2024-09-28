331bce20e2b9e033a6919ea104241dbebb5efa52
yuekeyuan <yuekeyuan@tom.com>
2024-09-28 21:54:11 +0800

    submit code

--- src/core/bean/IBeanInterface.h
+++ src/core/bean/IBeanInterface.h
@@ -10,12 +10,9 @@
 
 $PackageWebCoreBegin
 
-
 template<class T, bool enabled = true>
 class IBeanInterface : public IBeanWare, public ITaskInstantUnit<T, enabled>, public ITraceUnit<T, false>
 {
-    template<typename U, bool>
-    friend class ITaskIstantUnit;
 public:
     IBeanInterface() = default;
     virtual ~IBeanInterface() = default;
