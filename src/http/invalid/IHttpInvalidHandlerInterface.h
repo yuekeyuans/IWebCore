6c158ad2ae67270dc607144455b4e3ef855e42a5
yuekeyuan <yuekeyuan@tom.com>
2024-11-07 20:39:43 +0800

    submit code

--- src/http/invalid/IHttpInvalidHandlerInterface.h
+++ src/http/invalid/IHttpInvalidHandlerInterface.h
@@ -17,8 +17,15 @@ template<typename T, typename Invalid, bool enabled=true>
 class IHttpInvalidHandlerInterface : public IHttpInvalidHandlerWare, public ITaskWareUnit<T, IHttpTaskCatagory, enabled>, public ISingletonUnit<T>
 {
 public:
+    virtual double $order() const override;
     virtual void $task() final;
 };
+
+template<typename T, typename Invalid, bool enabled>
+double IHttpInvalidHandlerInterface<T, Invalid, enabled>::$order() const
+{
+    return 10;
+}
 
 template<typename T, typename Invalid, bool enabled>
 void IHttpInvalidHandlerInterface<T, Invalid, enabled>::$task()
