5b912e06e4adca9ddffd426a468c9c4745442a9c
岳克远 <60599728+yuekeyuans@users.noreply.github.com>
2024-12-12 00:10:54 +0800

    submit code

--- src/http/controller/pp/IControllerPreProcessor.h
+++ src/http/controller/pp/IControllerPreProcessor.h
@@ -12,21 +12,21 @@ private:
 #ifdef Q_MOC_RUN
     #define $Path(name)       name##_$path
     #define $Query(name)      name##_$query
-    #define $Body(name)       name##_$body
-    #define $Content(name)    name##_$content
     #define $Header(name)     name##_$header
     #define $Cookie(name)     name##_$cookie
     #define $Session(name)    name##_$session
-    #define $Optional(name)     name##_$optional
+    #define $Form(name)       name##_$body
+    #define $Json(name)       name##_$json
+    #define $Optional(name)   name##_$optional
 
 #else
     #define $Query(name)      name
     #define $Path(name)       name
-    #define $Body(name)       name
-    #define $Content(name)    name
     #define $Header(name)     name
     #define $Cookie(name)     name
     #define $Session(name)    name
+    #define $Form(name)       name
+    #define $Json(name)       name
     #define $Optional(name)   name
 
 #endif
